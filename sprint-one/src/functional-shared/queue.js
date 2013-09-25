ac06c288a8f69df7c0ce06adb34f073ffaee9ed6
Hack Reactor Students <pieter.a.dejong@gmail.com>
2013-09-25 15:28:57 -0700

    finished queue for functional shared method

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,6 +1,32 @@
 var makeQueue = function(){
   // Hey! Copy your code from src/functional/queue.js and paste it here
+  var instance = {};
+
+  // Use an object with numeric keys to store values
+  instance.last = 0;
+  instance.look = 0;
+  _.extend(instance, queueMethods);
+  // Implement the methods below
+  return instance;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  enqueue : function(value){
+    this[this.last] = value;
+    this.last++;
+  },
+
+  dequeue : function(){
+    if (this.last - this.look) {
+      var temp = this[this.look];
+      delete this[this.look];
+      this.look++;
+      return temp;
+    }
+  },
+
+  size : function(){
+    return this.last - this.look;
+  }
+};
 
