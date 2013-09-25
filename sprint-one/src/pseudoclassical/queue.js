9609452b4ffcb0fe5864db3cb5ebc40f1fba16e6
Hack Reactor Students <pieter.a.dejong@gmail.com>
2013-09-25 16:05:25 -0700

    created stack and queue functions using pseudoclassical object creation

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,4 +1,24 @@
 var Queue = function() {
-  // Hey! Copy your code from src/prototypal/queue.js and paste it here
+  this.last = 0;
+  this.look = 0;
 };
 
+Queue.prototype.enqueue = function(value){
+  this[this.last] = value;
+  this.last++;
+};
+
+Queue.prototype.dequeue = function(){
+  if (this.last - this.look) {
+    var temp = this[this.look];
+    delete this[this.look];
+    this.look++;
+    return temp;
+  }
+};
+
+Queue.prototype.size = function(){
+  return this.last - this.look;
+};
+
+var makeQueue = new Queue();
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,4 +1,23 @@
 var Stack = function() {
-  // Hey! Copy your code from src/prototypal/stack.js and paste it here
+  this.sizeVal = 0;
 };
 
+Stack.prototype.push = function(value){
+  this[this.sizeVal] = value;
+  this.sizeVal++;
+};
+
+Stack.prototype.pop = function(){
+  if (this.sizeVal) {
+    var temp = this[this.sizeVal - 1];
+    delete this[this.sizeVal - 1];
+    this.sizeVal--;
+    return temp;
+  }
+};
+
+Stack.prototype.size = function(){
+  return this.sizeVal;
+};
+
+var makeStack = new Stack();
