ad3cae942fd4a1d8c73d5f75f44c99f114a3f05b
Hack Reactor Students <pieter.a.dejong@gmail.com>
2013-09-25 15:39:21 -0700

    created a prototypal stack

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,6 +1,30 @@
 var makeStack = function() {
-  // Hey! Copy your code from src/functional-shared/stack.js and paste it here
+  // Hey! Copy your code from src/functional/stack.js and paste it here
+  // Use an object with numeric keys to store values
+  var instance = Object.create(stackMethods);
+  instance.sizeVal = 0;
+  // Implement the methods below
+  return instance;
 };
 
-var stackMethods = {};
+var stackMethods = {
 
+  push : function(value){
+    this[this.sizeVal] = value;
+    this.sizeVal++;
+  },
+
+  pop : function(){
+    if (this.sizeVal) {
+      var temp = this[this.sizeVal - 1];
+      delete this[this.sizeVal - 1];
+      this.sizeVal--;
+      return temp;
+    }
+  },
+
+  size : function(){
+    return this.sizeVal;
+  }
+
+};
