bb199346ae283559d05dd828129afe223389f123
stangah <pstanger@gmail.com>
2013-09-25 13:31:51 -0700

    implemented a queu e

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,28 @@ var makeQueue = function(){
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var last = 0;
+  var look = 0;
+  var temp;
 
   // Implement the methods below
 
   instance.enqueue = function(value){
+    storage[last] = value;
+    last++;
   };
 
   instance.dequeue = function(){
+    if (last - look) {
+      temp = storage[look];
+      delete storage[look];
+      look++;
+      return temp;
+    }
   };
 
   instance.size = function(){
+    return last - look;
   };
 
   return instance;
