cdbdd41c040024766ca7e9403e0ddfc998545459
stangah <pstanger@gmail.com>
2013-09-25 14:34:32 -0700

    added functional shared stack

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,6 +1,34 @@
 var makeStack = function() {
   // Hey! Copy your code from src/functional/stack.js and paste it here
+  var instance = {};
+  var storage = {};
+  instance.sizeNum = 0;
+  var temp;
+  _.extend(instance, stackMethods);
+  return instance;
 };
 
-var stackMethods = {};
+var stackMethods = {
+
+
+  // Implement the methods below
+
+  push : function(value){
+    this[this.sizeNum] = value;
+    this.sizeNum++;
+  },
+
+  pop : function(){
+    if (this.sizeNum) {
+      temp = this[this.sizeNum - 1];
+      delete this[this.sizeNum - 1];
+      this.sizeNum--;
+      return temp;
+    }
+  },
+
+  size : function(){
+    return this.sizeNum;
+  }
+};
 
