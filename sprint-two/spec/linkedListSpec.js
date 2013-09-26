71809a7e95793b72ea76641a934fc77fa2081975
stangah <pstanger@gmail.com>
2013-09-25 21:53:25 -0700

    created linked list function and added various tests of it

--- sprint-two/spec/linkedListSpec.js
+++ sprint-two/spec/linkedListSpec.js
@@ -16,5 +16,74 @@ describe("linkedList", function() {
     expect(linkedList.contains).toEqual(jasmine.any(Function));
   });
 
-  // add more tests here to test the functionality of linkedList
+  it ("should return null when trying to remove the head of an empty list", function() {
+    expect(linkedList.removeHead()).toEqual(null);
+  });
+
+  it("should have the same head and tail after adding a tail to an empty list", function() {
+    linkedList.addToTail(85);
+    expect(linkedList.head).toEqual(linkedList.tail);
+  });
+
+  it("should contain values that exist between the head and tail", function() {
+    linkedList.addToTail(10);
+    linkedList.addToTail(20);
+    linkedList.addToTail(30);
+    expect(linkedList.contains(10)).toEqual(true);
+    expect(linkedList.contains(20)).toEqual(true);
+    expect(linkedList.contains(30)).toEqual(true);
+  });
+
+  it("should not contain values that were not added", function() {
+    linkedList.addToTail(1);
+    linkedList.addToTail(5);
+    linkedList.addToTail(1000);
+    expect(linkedList.contains(4)).toEqual(false);
+    expect(linkedList.contains(5)).toEqual(true);
+  });
+
+  it("should not contain values that were added and then removed", function() {
+    linkedList.addToTail(1);
+    linkedList.removeHead();
+    linkedList.addToTail(1000);
+    expect(linkedList.contains(1)).toEqual(false);
+    expect(linkedList.contains(1000)).toEqual(true);
+  });
+
+  it("should be able to add and then find null values", function() {
+    linkedList.addToTail(6);
+    linkedList.addToTail(null);
+    linkedList.addToTail(90);
+    expect(linkedList.contains(null)).toEqual(true);
+  });
+
+  it("should not find null values if list doesn't contain them", function() {
+    linkedList.addToTail(1);
+    linkedList.addToTail(2);
+    linkedList.addToTail(10);
+    expect(linkedList.contains(null)).toEqual(false);
+  });
+
+  it("should handle integers, strings, booleans, objects and functions", function() {
+    var fnc = function () {};
+    var obj = {};
+    linkedList.addToTail(3);
+    linkedList.addToTail("three");
+    linkedList.addToTail(true);
+    linkedList.addToTail(fnc);
+    linkedList.addToTail(obj);
+    expect(linkedList.contains(3)).toEqual(true);
+    expect(linkedList.contains("three")).toEqual(true);
+    expect(linkedList.contains(true)).toEqual(true);
+    expect(linkedList.contains(fnc)).toEqual(true);
+    expect(linkedList.contains(obj)).toEqual(true);
+  });
+
+  /*
+  it("", function() {
+    expect().toEqual();
+  });
+  */
+
+
 });
--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,13 +4,38 @@ var makeLinkedList = function(){
   list.head = null;
   list.tail = null;
 
-  list.addToTail = function(){
+  list.addToTail = function(value){
+    if (list.tail) {
+      list.tail.next = makeNode(value);
+      list.tail = list.tail.next;
+    } else {
+      list.tail = makeNode(value);
+      list.head = list.tail;
+    }
   };
 
   list.removeHead = function(){
+    if (list.head) {
+      var temp = list.head.value;
+      if (list.head === list.tail) {list.tail = null;} // in the case where there is only one node in the list
+      list.head = list.head.next;
+      return temp;
+    } else {
+      return null;
+    }
   };
 
-  list.contains = function(){
+  list.contains = function(target){
+    if (list.head) {
+      var current = list.head;
+      do {
+        if (current.value === target) {
+          return true;
+        }
+        current = current.next;
+      } while (current !== null);
+      return false;
+    } else { return false; }
   };
 
   return list;
