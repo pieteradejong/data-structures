9b2733e2b3cb7ce0104a6f77235ec9ca842df2bb
stangah <pstanger@gmail.com>
2013-09-27 18:28:58 -0700

    Writing binary search tree with Pieter

--- sprint-two/spec/binarySearchTreeSpec.js
+++ sprint-two/spec/binarySearchTreeSpec.js
@@ -6,9 +6,28 @@ describe("binarySearchTree", function() {
   });
 
   it("should have methods named 'insert', 'contains', and 'depthFirstLog", function() {
-    expect(binarySearchTree.children).toEqual(jasmine.any(Function));
+    expect(binarySearchTree.insert).toEqual(jasmine.any(Function));
     expect(binarySearchTree.contains).toEqual(jasmine.any(Function));
     expect(binarySearchTree.depthFirstLog).toEqual(jasmine.any(Function));
   });
-  // add more tests here to test the functionality of binarySearchTree
+
+  it("should contain values that were inserted", function() {
+    binarySearchTree.insert(10);
+    expect(binarySearchTree.contains(10)).toEqual(true);
+  });
+
+  it("should contain modified values", function() {
+    var test = [];
+    binarySearchTree.insert(10);
+    binarySearchTree.insert(5);
+    binarySearchTree.insert(12);
+    expect(binarySearchTree.contains(10)).toEqual(true);
+    binarySearchTree.depthFirstLog(function(i) {test.push(i);} );
+    expect(test).toEqual([5,10,12]);
+  });
+
+  // it("should contain values that were inserted", function() {
+  //   binarySearchTree.insert(10);
+  //   expect(binarySearchTree.contains(10)).toEqual(true);
+  // });
 });
--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,2 +1,66 @@
-var makeBinarySearchTree = function(){
+var makeBinarySearchTree = function(val){
+  var tree = {};
+  tree.parent = null;
+  tree.left = null;
+  tree.right=null;
+  tree.value = val || null;
+  for (var prop in binarySearchTreeProperties) {
+    tree[prop] = binarySearchTreeProperties[prop];
+  }
+  return tree;
 };
+
+var binarySearchTreeProperties = {};
+
+binarySearchTreeProperties.insert = function(val) {
+
+  // In the case where the root node is empty
+  if (this.value === null) { this.value = val; }
+
+  // After initializing the root node
+  else if (val < this.value) {
+    if (this.left === null) {
+      this.left = makeBinarySearchTree(val);
+      this.left.parent = this;
+    } else { this.left.insert(val); }
+  } else if (val > this.value) {
+    if (this.right === null) {
+      this.right = makeBinarySearchTree(val);
+      this.right.parent = this;
+    } else { this.right.insert(val); }
+  }
+};
+
+binarySearchTreeProperties.contains = function(target) {
+  if (this.value === target){ return true; }
+  else if (target < this.value) {
+    if (this.left !== null) { return this.left.contains(target); }
+  } else {
+    if (this.right !== null) { return this.right.contains(target); }
+  }
+  return false;
+};
+
+binarySearchTreeProperties.depthFirstLog = function(callback) {
+  this.left !== null && this.left.depthFirstLog(callback);
+  callback(this.value);
+  this.right !== null && this.right.depthFirstLog(callback);
+};
+
+binarySearchTreeProperties.breadthFirstLog = function(callback) {
+  var q = newQueue();
+  q.enqueue(this);
+
+  while(q.size()) {
+    var item = q.dequeue();
+    callback(item.value);
+    item.left && q.enqueue(item.left);
+    item.right && q.enqueue(item.right);
+  }
+
+};
+
+
+
+
+
